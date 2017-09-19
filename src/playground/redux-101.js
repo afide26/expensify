c9a9c6de7ac0991ea6d7338ee580836504932ba0
Alan Fidelino <afidedev26@gmail.com>
2017-09-19 20:37:08 +1000

    Finish Basic Redux lessons

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -4,9 +4,16 @@ const store = createStore((state={count:0}, action)=>{
 
   switch(action.type){
     case 'INCREMENT':
-     return { count: state.count + 1};
+      const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1
+     return { count: state.count + incrementBy};
+
     case 'DECREMENT':
-     return state.count >=1 ? { count: state.count -1} : 'Value is below 1';
+     const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
+     return state.count >=1 ? { count: state.count - decrementBy} : 'Value is below 1';
+    
+    case 'SET':
+      return {count: action.count};
+
     case 'RESET':
      return { count: 0}
     default:
@@ -15,35 +22,54 @@ const store = createStore((state={count:0}, action)=>{
 
 });
 
-console.log(store.getState());
+// State Getter
+// Watch and listen to state
+const unsubscribe = store.subscribe(()=>{
+  console.log(store.getState());
+})
+
 
 // Actions - object that get sent to the store
+// SET
+//SET
+store.dispatch({
+  type:'SET',
+  count: 101
+});
+
+// Increment
+store.dispatch({
+  type:'INCREMENT',
+  incrementBy: 50
+});
+
+// unsubscribe();
+
+// Decrement
+store.dispatch({
+  type:'DECREMENT',
+  decrementBy: 5
+})
+
+
 // Increment
 store.dispatch({
   type:'INCREMENT'
 });
-
-
-
-
-// Decrement
 store.dispatch({
-  type:'DECREMENT'
-})
+  type:'INCREMENT'
+});
 
 // Reset
 store.dispatch({
   type:'RESET'
 });
 
-// store.dispatch({
-//   type:'INCREMENT'
-// });
-// store.dispatch({
-//   type:'INCREMENT'
-// });
+//SET
+store.dispatch({
+  type:'SET',
+  count: 101
+})
 
 
 
-// State Getter
-console.log(store.getState());
